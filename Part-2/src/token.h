76179054500862abbce3756c207ff5d4d2510734
juid <dennis.nik@protonmail.com>
2021-04-07 22:24:46 +0300

    Started working on second part, made good progress

---
+++ Part-2/src/token.h
@@ -0,0 +1,11 @@
+#define DELIMITER 1
+#define INTCONST 2
+#define FLOAT 3
+#define STRING 4
+#define IDENTIFIER 5
+#define VARIABLE 6
+#define COMMENT 7
+#define WHITE_SPACES 8
+#define OPERATOR 9
+#define KEYWORD 10
+/*#define FILL ME */
